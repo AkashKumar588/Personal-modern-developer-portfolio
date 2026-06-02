41ea9f34ca4bf1eaa10d5ca39ace8df2515d89ff
AkashKumar588 <akashkumar.024026@gmail.com>
2026-06-03 00:00:44 +0530

    fix lucide-react bug

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()], //, tailwindcss(), // viteSingleFile()],
-
-  
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
